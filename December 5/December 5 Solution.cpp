a5b2d7d72cf5b798714badea709a6a4851bf9ab2
Sanjay <sanjays2402@gmail.com>
2020-12-05 17:49:54 +0530

    Update December 5 Solution.cpp

--- December 5/December 5 Solution.cpp
+++ December 5/December 5 Solution.cpp
@@ -2,35 +2,18 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         
-        int m = flowerbed.size();
-        if(m == 0)
-            return false;
-        if(m == 1)
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+        
+        for(int i=1; i < flowerbed.size()-1; i++)
         {
-            if(n == 0)
-                return true;
-            if(n > 1 || flowerbed[0] == 1)
-                return false;
-            return true;
-        }
-        if(flowerbed[0] == 0 && flowerbed[1] == 0)
-        {
-            flowerbed[0] = 1;
-            n--;
-        }
-        for(int i=2; i<m-2; i++)
-        {
-            if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)
+            if(flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0)
             {
-                n--;
                 flowerbed[i] = 1;
+                n--;
             }
         }
-        if(flowerbed[m-1] == 0 && flowerbed[m-2] == 0)
-        {
-            flowerbed[m-1] = 1;
-            n--;
-        }
+        
         return n <= 0;
     }
 };
